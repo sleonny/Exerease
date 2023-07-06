4ad1c42a2459ecb9b207eea56221ef5881d7d966
Michelle Correia <michellecorreia@Michelles-MacBook-Pro.local>
2023-07-06 19:37:51 -0400

    first commit

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Auth from '../../utils/auth';
+
+
+
+export default Header;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import ProfileList from '../components/ProfileList';
+
+import { QUERY_PROFILES } from '../utils/queries';
+
+
+export default Home;
