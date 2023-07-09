0379497a47b7fc29632c7ae0ef03ce27a8a7b4cd
Michelle Correia <michellecorreia@Michelles-MacBook-Pro.local>
2023-07-09 17:11:02 -0400

    added login page, sign in page, header and footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,1 +1,25 @@
 import React from 'react';
+
+const Footer = () => {
+  const footerStyle = {
+    backgroundColor: '#f5f5f5',
+    padding: '20px',
+    textAlign: 'center',
+    borderTop: '1px solid #ccc',
+  };
+
+  const textStyles = {
+    color: '#555',
+    fontSize: '14px',
+  };
+
+  return (
+    <footer style={footerStyle}>
+      <p style={textStyles}>
+        &copy; {new Date().getFullYear()} Exerease. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,102 +1,108 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import { useMutation } from '@apollo/client';
-import { ADD_PROFILE } from '../utils/mutations';
+import { SIGNUP_USER } from '../utils/mutations';
 
-import Auth from '../utils/auth';
+const SignupForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signup, { error, data }] = useMutation(SIGNUP_USER);
 
-const Signup = () => {
-  const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
-  const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
-
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(formState);
 
     try {
-      const { data } = await addProfile({
-        variables: { ...formState },
+      const { data } = await signup({
+        variables: { email, password },
       });
 
-      Auth.login(data.addProfile.token);
+      // Handle successful signup
+      console.log(data);
     } catch (e) {
       console.error(e);
     }
+
+    // Clear form values
+    setEmail('');
+    setPassword('');
+  };
+
+  const containerStyle = {
+    maxWidth: '400px',
+    margin: '0 auto',
+    padding: '20px',
+    backgroundColor: '#f5f5f5',
+    borderRadius: '5px',
+    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
+    backgroundImage: 'url(/workout-background.jpg)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
+
+  const inputStyle = {
+    width: '100%',
+    padding: '10px',
+    marginBottom: '10px',
+    border: '1px solid #ccc',
+    borderRadius: '3px',
+  };
+
+  const buttonStyle = {
+    display: 'block',
+    width: '100%',
+    padding: '10px',
+    border: 'none',
+    borderRadius: '3px',
+    backgroundColor: '#007bff',
+    color: '#fff',
+    fontSize: '16px',
+    cursor: 'pointer',
+  };
+
+  const buttonHoverStyle = {
+    ...buttonStyle,
+    backgroundColor: '#0056b3',
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your username"
-                  name="name"
-                  type="text"
-                  value={formState.name}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-info"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
+    <div style={containerStyle}>
+      <h4>Sign Up</h4>
+      <form onSubmit={handleSubmit}>
+        <input
+          style={inputStyle}
+          type="email"
+          placeholder="Your email"
+          value={email}
+          onChange={handleChangeEmail}
+        />
+        <input
+          style={inputStyle}
+          type="password"
+          placeholder="******"
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <button style={buttonStyle} type="submit">Sign Up</button>
+      </form>
 
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </main>
+      {data && (
+        <p>
+          Success! You may now head{' '}
+          <Link to="/">back to the homepage.</Link>
+        </p>
+      )}
+
+      {error && <p>{error.message}</p>}
+    </div>
   );
 };
 
-export default Signup;
+export default SignupForm;
