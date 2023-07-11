1ad66e93bd5076b09c78a3916171e2936d3060a4
Sean L <leonardmsean@gmail.com>
2023-07-10 20:34:52 -0400

    added .env, debugged resolvers and typeDefs, updated index.js inside schemas folder

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,29 @@
-const typeDefs = require("./typeDefs");
-const resolvers = require("./resolvers");
+const { mergeTypeDefs } = require("@apollo/client");
+
+const goalType = require("./typeDefs/GoalType");
+const userType = require("./typeDefs/UserType");
+const workoutHistoryType = require("./typeDefs/WorkoutHistoryType");
+const workoutPlanType = require("./typeDefs/WorkoutPlanType");
+
+const typeDefs = mergeTypeDefs([
+  goalType,
+  userType,
+  workoutHistoryType,
+  workoutPlanType,
+]);
+
+const { mergeResolvers } = require("@graphql-tools/merge");
+
+const goalResolver = require("./Resolvers/GoalResolver");
+const userResolver = require("./Resolvers/GoalResolver");
+const workoutHistoryResolver = require("./Resolvers/WorkoutHistoryResolver");
+const workoutPlanResolver = require("./Resolvers/WorkoutPlanResolver");
+
+const resolvers = mergeResolvers([
+  goalResolver,
+  userResolver,
+  workoutHistoryResolver,
+  workoutPlanResolver,
+]);
 
 module.exports = { typeDefs, resolvers };
--- server/schemas/typeDefs/WorkoutPlanType.js
+++ server/schemas/typeDefs/WorkoutPlanType.js
@@ -7,19 +7,36 @@ const WorkoutPlanType = gql`
     sets: Int
     reps: Int
     duration: Int
+    type: String
+    muscle: String
+    equipment: String
+    difficulty: String
+    instructions: String
   }
 
   type WorkoutPlan {
     id: ID!
     name: String!
     description: String
+    muscleType: String!
     exercises: [Exercise]
     duration: String!
+  }
+
+  type ExternalExercise {
+    name: String
+    type: String
+    muscle: String
+    equipment: String
+    difficulty: String
+    instructions: String
+
   }
 
   extend type Query {
     workoutPlan(id: ID!): WorkoutPlan
     workoutPlans: [WorkoutPlan]
+    searchExercises(muscle: String!): [ExternalExercise]
   }
 `;
 
