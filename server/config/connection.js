2fe854dce3ae2eaf54be207fe5c656352b4acb66
Japheth <japhethbruce93@gmail.com>
2023-07-12 13:01:15 -0500

    establish mongo connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const MONGODB_URI = process.env.MONGOURI;
 
 
-mongoose.connect(process.env.MONGODB_URI); 
+mongoose.connect(MONGODB_URI, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((error) => console.log("Error connecting to MongoDB:", error));
 
 module.exports = mongoose.connection
-
