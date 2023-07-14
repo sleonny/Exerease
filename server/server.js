bba0da85284b4533c50d9ac6fab62c7eefd46009
Japheth <japhethbruce93@gmail.com>
2023-07-14 07:50:37 -0500

    added fronted routes to server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require('path')
 const db = require("./config/connection");
-const {authMiddleWare } = require("./utils/auth")
+const { authMiddleWare } = require("./utils/auth")
 
 // Import the typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -22,11 +22,22 @@ const server = new ApolloServer({
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.use("/images", express.static(path.join(__dirname,"../client/images")))
+// serve static images on page load
+// TO DO: move client side images into images folder
+app.use("/images", express.static(path.join(__dirname,"../client/images")));
 
+// Start production build
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../client/build")))
+}
+
+// serves up index.html
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
 
 // Middleware for Express and Apollo
-server.applyMiddleware({ app });
+// server.applyMiddleware({ app });
 
 // Connection to MongoDB
 // mongoose
