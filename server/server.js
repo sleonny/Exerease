65b2a11ec760796973bb4a068bdb8b0bfaf2b740
Japheth <japhethbruce93@gmail.com>
2023-07-14 07:32:33 -0500

    fixed server

--- server/server.js
+++ server/server.js
@@ -1,34 +1,50 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const mongoose = require("./config/connection");
+const path = require('path')
+const db = require("./config/connection");
+const {authMiddleWare } = require("./utils/auth")
 
 // Import the typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+
+// Set the port
+const PORT = process.env.PORT || 4000;
+// Setup Express
+const app = express();
 
 // Setup the Apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req }),
+  context: authMiddleWare,
 });
 
-// Setup Express
-const app = express();
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 
 // Middleware for Express and Apollo
 server.applyMiddleware({ app });
 
 // Connection to MongoDB
-mongoose
-  .once("open", () => console.log("Connected to MongoDB"))
-  .on("error", (error) => console.log("Error connecting to MongoDB:", error));
+// mongoose
+//   .once("open", () => console.log("Connected to MongoDB"))
+//   .on("error", (error) => console.log("Error connecting to MongoDB:", error));
 
-// Set the port
-const PORT = process.env.PORT || 4000;
+const startApolloServer = async ()=> {
+  await server.start();
+  server.applyMiddleware({app});
+  db.once("open", ()=>{
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`API server running on port: ${PORT}`);
+      console.log(
+        `GraphQL is running on http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  })
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(
-    `Server is running on http://localhost:${PORT}${server.graphqlPath}`
-  );
-});
+}
+
+startApolloServer()
+
