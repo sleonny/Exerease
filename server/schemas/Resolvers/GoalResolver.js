9e0c863130ee97ebb49a9aef1a4f1a8f261f90c7
Sean L <leonardmsean@gmail.com>
2023-07-17 19:45:18 -0400

    got server running :-)

--- server/schemas/Resolvers/GoalResolver.js
+++ server/schemas/Resolvers/GoalResolver.js
@@ -1,8 +1,6 @@
 const { gql } = require("apollo-server-express");
 const { Goals } = require("../../models/Goals.js");
 
-
-
 const resolvers = {
   Query: {
     workoutGoal: async (parent, { id }, context) => {
@@ -13,17 +11,16 @@ const resolvers = {
     },
   },
 
-
-Mutation: {
-  createWorkoutGoal: async (parent, { input }, context) => {
-    return await WorkoutGoal.create(input);
+  Mutation: {
+    createworkoutGoal: async (parent, { input }, context) => {
+      return await WorkoutGoal.create(input);
+    },
+    updateworkoutGoal: async (parent, { id, input }, context) => {
+      return await WorkoutGoal.findByIdAndUpdate(id, input, { new: true });
+    },
+    deleteworkoutGoal: async (parent, { id }, context) => {
+      return await WorkoutGoal.findByIdAndDelete(id);
+    },
   },
-  updateWorkoutGoal: async (parent, { id, input }, context) => {
-    return await WorkoutGoal.findByIdAndUpdate(id, input, { new: true });
-  },
-  deleteWorkoutGoal: async (parent, { id }, context) => {
-    return await WorkoutGoal.findByIdAndDelete(id);
-  },
-},
-}  
-  module.exports = resolvers;
+};
+module.exports = resolvers;
--- server/schemas/Resolvers/UserResolver.js
+++ server/schemas/Resolvers/UserResolver.js
@@ -1,34 +1,27 @@
 const { gql } = require("apollo-server-express");
 const User = require("../../models/User.js"); // Corrected import
 
-
-
-
-
 const resolvers = {
-    Query: {
-      
-      user: async (parent, args, context) => {
-        
-        const { userId } = args;
-        const user = await context.dataSources.getUserById(userId);
-  
-        return user;
-      },
+  Query: {
+    user: async (parent, args, context) => {
+      const { userId } = args;
+      const user = await context.dataSources.getUserById(userId);
+
+      return user;
     },
-    
-    Mutation: {
-      createUser: async (parent, { input }, context) => {
-        return await User.create(input);
-      },
-      updateUser: async (parent, { id, input }, context) => {
-        return await User.findByIdAndUpdate(id, input, { new: true });
-      },
-      deleteUser: async (parent, { id }, context) => {
-        return await User.findByIdAndDelete(id);
-      },
-    }, 
-  };
-  
-  module.exports = resolvers;
-  
+  },
+
+  Mutation: {
+    addUser: async (parent, { input }, context) => {
+      return await User.create(input);
+    },
+    updateUser: async (parent, { id, input }, context) => {
+      return await User.findByIdAndUpdate(id, input, { new: true });
+    },
+    removeUser: async (parent, { id }, context) => {
+      return await User.findByIdAndDelete(id);
+    },
+  },
+};
+
+module.exports = resolvers;
