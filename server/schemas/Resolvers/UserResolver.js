75cbb298407999ef6be76ef5dbe8a56f781a48fd
Sean L <leonardmsean@gmail.com>
2023-07-18 15:28:02 -0400

    trying route with jws for first time

--- server/schemas/Resolvers/UserResolver.js
+++ server/schemas/Resolvers/UserResolver.js
@@ -1,5 +1,34 @@
+// const { gql } = require("apollo-server-express");
+// const User = require("../../models/User.js"); // Corrected import
+// const jwt = require('jsonwebtoken');
+
+// const userResolvers = {
+//   Query: {
+//     user: async (parent, args, context) => {
+//       const { userId } = args;
+//       const user = await context.dataSources.getUserById(userId);
+
+//       return user;
+//     },
+//   },
+//   Mutation: {
+//     addUser: async (parent, { user, name, email, password }, context) => {
+//       return await User.create({ user, name, email, password });
+//     },
+//     updateUser: async (parent, { id, input }, context) => {
+//       return await User.findByIdAndUpdate(id, input, { new: true });
+//     },
+//     removeUser: async (parent, { id }, context) => {
+//       return await User.findByIdAndDelete(id);
+//     },
+//   },
+// };
+
+// module.exports = userResolvers;
+
 const { gql } = require("apollo-server-express");
 const User = require("../../models/User.js"); // Corrected import
+const jwt = require("jsonwebtoken"); // import jsonwebtoken
 
 const userResolvers = {
   Query: {
@@ -12,7 +41,25 @@ const userResolvers = {
   },
   Mutation: {
     addUser: async (parent, { user, name, email, password }, context) => {
-      return await User.create({ user, name, email, password });
+      const newUser = await User.create({ user, name, email, password });
+
+      // JWT creation
+      const token = jwt.sign(
+        {
+          id: newUser._id,
+          email: newUser.email,
+          user: newUser.user,
+        },
+        "secret", // Replace 'secret' with your secret key or environment variable
+        {
+          expiresIn: "24h", // token will expire in 24 hours
+        }
+      );
+
+      newUser.tokens = newUser.tokens.concat({ token });
+      await newUser.save();
+
+      return { user: newUser, token };
     },
     updateUser: async (parent, { id, input }, context) => {
       return await User.findByIdAndUpdate(id, input, { new: true });
