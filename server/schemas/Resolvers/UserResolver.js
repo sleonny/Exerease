5a2aa92bcc0b14760b10d66c05f29476441fc96f
Sean L <leonardmsean@gmail.com>
2023-07-18 15:35:09 -0400

    working on post user route

--- server/schemas/Resolvers/UserResolver.js
+++ server/schemas/Resolvers/UserResolver.js
@@ -1,31 +1,3 @@
-// const { gql } = require("apollo-server-express");
-// const User = require("../../models/User.js"); // Corrected import
-// const jwt = require('jsonwebtoken');
-
-// const userResolvers = {
-//   Query: {
-//     user: async (parent, args, context) => {
-//       const { userId } = args;
-//       const user = await context.dataSources.getUserById(userId);
-
-//       return user;
-//     },
-//   },
-//   Mutation: {
-//     addUser: async (parent, { user, name, email, password }, context) => {
-//       return await User.create({ user, name, email, password });
-//     },
-//     updateUser: async (parent, { id, input }, context) => {
-//       return await User.findByIdAndUpdate(id, input, { new: true });
-//     },
-//     removeUser: async (parent, { id }, context) => {
-//       return await User.findByIdAndDelete(id);
-//     },
-//   },
-// };
-
-// module.exports = userResolvers;
-
 const { gql } = require("apollo-server-express");
 const User = require("../../models/User.js"); // Corrected import
 const jwt = require("jsonwebtoken"); // import jsonwebtoken
@@ -55,6 +27,8 @@ const userResolvers = {
           expiresIn: "24h", // token will expire in 24 hours
         }
       );
+
+      return { user: newUser, token };
 
       newUser.tokens = newUser.tokens.concat({ token });
       await newUser.save();
