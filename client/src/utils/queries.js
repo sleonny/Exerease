59fcdade5514f44d5eae6ebfa666ac4b0daccd40
Michelle Correia <michellecorreia@Michelles-MacBook-Pro.local>
2023-07-18 23:39:07 -0400

    added queries and mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -20,14 +20,58 @@ export const QUERY_SINGLE_PROFILE = gql`
   }
 `;
 
-export const GET_WORKOUT_HISTORY = gql`
-  query WorkoutHistory($workoutHistoryId: ID!) {
-  workoutHistory(id: $workoutHistoryId) {
+export const GET_WORKOUT_HISTORY = gql `
+query GetWorkoutHistory($workoutHistoryId: ID!) {
+workoutHistory(id: $workoutHistoryId) {
+  id
+  date
+  type
+  duration
+  calories
+}
+}
+`
+
+
+export const GET_WORKOUT_GOAL = gql `
+query GetWorkoutGoal($workoutGoalId: ID!) {
+  WorkoutGoal(id: $workoutGoalId) {
     id
-    date
     type
+    date
     duration
-    calories
+  }
+}`
+
+export const SEARCH_EXERCISES = gql `
+query SearchExercises($muscle: String!) {
+  searchExercises(muscle: $muscle) {
+    name
+    type
+    muscle
+    equipment
+    difficulty
+    instructions
+  }
+}
+`
+
+  export const GET_USER = gql `
+  query user(userId: $userId) {
+    _id
+    user
+    name
+    email
+    password
+  }
+  `
+  export const GET_WORKOUTGOALS = gql `
+  query GetAllWorkoutGoals {
+  workoutGoals {
+    id
+    type
+    date
+    duration
   }
 }
 `
