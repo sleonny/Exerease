5bbfa290269b094bc12643aad1a47ee344050d49
Japheth <japhethbruce93@gmail.com>
2023-07-20 10:39:12 -0500

    workouthistory data

--- client/src/pages/WorkoutHistory.js
+++ client/src/pages/WorkoutHistory.js
@@ -6,3 +6,19 @@ function WorkoutHistoryPage() {
   }, []);
   const handleSelectWorkoutHistory = (selectedWorkout) => {
   };
+
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", margin: "2rem" }}>
+      <h1>Workout History</h1>
+      <div>
+        {workoutHistory.map((workout) => (
+          <div
+            key={workout.id}
+            onClick={() => handleSelectWorkoutHistory(workout)}
+            style={{
+              background: "#f4f4f4",
+              padding: "1rem",
+              marginBottom: "1rem",
+              cursor: "pointer",
+            }}
+          >
