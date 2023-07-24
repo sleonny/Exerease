ab118cc97dc2449915988a3c8f3c90761c259658
Sean L <leonardmsean@gmail.com>
2023-07-24 19:12:42 -0400

    added typedefs for contactus

---
+++ server/schemas/typeDefs/ContactType.js
@@ -0,0 +1,22 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type ContactUs {
+    name: String!
+    email: String!
+    phone: String
+    message: String!
+    date: Date!
+  }
+
+  type Mutation {
+    submitContactUsForm(
+      name: String!
+      email: String!
+      phone: String
+      message: String!
+    ): ContactUs
+  }
+`;
+
+module.exports = contactUsTypeDefs;
