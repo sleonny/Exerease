745b401160e6fd7c8f8c20836a54427e0fe2c51d
Sean L <leonardmsean@gmail.com>
2023-07-24 15:41:43 -0400

    almost there

--- client/src/pages/WorkoutPlan.js
+++ client/src/pages/WorkoutPlan.js
@@ -1,138 +1,7 @@
-// import React, { useState, useEffect } from "react";
-// import { useLazyQuery } from "@apollo/client";
-// import { SEARCH_EXERCISES } from "../utils/queries";
-
-// export const SearchWorkoutPlan = () => {
-//   const [muscle, setMuscle] = useState("");
-//   const [searchExercises, { loading, data, error }] =
-//     useLazyQuery(SEARCH_EXERCISES);
-//   const [exerciseData, setExerciseData] = useState(null);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     searchExercises({ variables: { muscle } });
-//   };
-
-//   useEffect(() => {
-//     if (data) {
-//       setExerciseData(data.searchExercises);
-//     }
-//   }, [data]);
-
-//   if (error) {
-//     return <p>Error: {error.message}</p>;
-//   }
-
-//   // Pass exerciseData to CreateWorkoutPlan
-//   return (
-//     <div style={{ position: "absolute", top: "10%", left: "10%" }}>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Search Muscle"
-//           value={muscle}
-//           onChange={(e) => setMuscle(e.target.value)}
-//         />
-//         <button type="submit">Search</button>
-//       </form>
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         exerciseData && (
-//           <div>
-//             <CreateWorkoutPlan exerciseData={exerciseData} />
-//           </div>
-//         )
-//       )}
-//     </div>
-//   );
-// };
-
-// export const CreateWorkoutPlan = ({ exerciseData }) => {
-//   const [newPlan, setNewPlan] = useState({
-//     name: "",
-//     description: "",
-//     muscleType: "",
-//     duration: "",
-//     exercises: [],
-//   });
-
-//   useEffect(() => {
-//     if (exerciseData) {
-//       setNewPlan((prevPlan) => ({ ...prevPlan, exercises: exerciseData }));
-//     }
-//   }, [exerciseData]);
-
-//   const [createWorkoutPlan, { data, error, loading }] =
-//     useMutation(CREATE_WORKOUT_PLAN);
-
-//   const handleInputChange = (e) => {
-//     setNewPlan({ ...newPlan, [e.target.name]: e.target.value });
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     createWorkoutPlan({ variables: { input: newPlan } });
-//   };
-
-//   if (loading) {
-//     return <p>Creating...</p>;
-//   }
-
-//   if (error) {
-//     return <p>Error: {error.message}</p>;
-//   }
-
-//   if (data) {
-//     return <p>Workout plan created successfully!</p>;
-//   }
-
-//   return (
-//     <div style={{ position: "absolute", top: "50%", left: "50%" }}>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           name="name"
-//           placeholder="Name"
-//           value={newPlan.name}
-//           onChange={handleInputChange}
-//         />
-//         <button type="submit">Create</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export const LoadSavedWorkoutPlan = () => {
-//   const { loading, data, error } = useQuery(GET_WORKOUT_PLANS);
-
-//   if (loading) {
-//     return <p>Loading...</p>;
-//   }
-
-//   if (error) {
-//     return <p>Error: {error.message}</p>;
-//   }
-
-//   return (
-//     <div style={{ position: "absolute", top: "90%", right: "10%" }}>
-//       {data?.workoutPlans.map((plan) => (
-//         <div key={plan.id || plan.name}>
-//           <h4>{plan.name}</h4>
-//           <p>{plan.description}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// File: pages/WorkoutPlan.js
-// File: pages/WorkoutPlan.js
-
-import React, { useState } from "react";
-import { useMutation, useQuery } from "@apollo/client";
+import React, { useState, useEffect } from "react";
+import { useMutation, useQuery, gql } from "@apollo/client";
+import { GET_WORKOUT_PLANS } from "../utils/queries";
 import { ADD_WORKOUT_PLAN } from "../utils/mutations";
-import { GET_WORKOUT_BY_NAME } from "../utils/queries";
 
 const WorkoutPlan = () => {
   const [workoutPlan, setWorkoutPlan] = useState({
@@ -152,11 +21,10 @@ const WorkoutPlan = () => {
   });
 
   const [addWorkoutPlan] = useMutation(ADD_WORKOUT_PLAN);
+  const { loading, error, data } = useQuery(GET_WORKOUT_PLANS);
 
-  // Define the query hook to fetch the workout plan by name
-  const { loading, error, data, refetch } = useQuery(GET_WORKOUT_BY_NAME, {
-    variables: { name: workoutPlan.name },
-  });
+  // Declare selectedWorkoutPlan state variable and its setter function
+  const [selectedWorkoutPlan, setSelectedWorkoutPlan] = useState(null);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -196,13 +64,17 @@ const WorkoutPlan = () => {
     }
   };
 
+  const handleWorkoutPlanClick = (workoutPlan) => {
+    setSelectedWorkoutPlan(workoutPlan);
+  };
+
   return (
     <div
       style={{
-        backgroundImage: "url('../../workout-background8.jpg')",
+        backgroundImage: "url('workout-background8.jpg')", // Assuming the image is in the public folder
         backgroundSize: "cover",
         backgroundPosition: "center",
-        minHeight: "100vh", // Ensure the background covers the entire viewport height
+        minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
@@ -210,37 +82,153 @@ const WorkoutPlan = () => {
         padding: "2rem",
       }}
     >
-      <h2>Create Workout Plan</h2>
-      <form onSubmit={handleSubmit}>
-        {/* ... input fields for creating workout plan ... */}
-      </form>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start", // Align children to the left
+          marginBottom: "2rem", // Add some margin at the bottom for spacing
+        }}
+      >
+        <h2>Create Workout Plan</h2>
+        <form onSubmit={handleSubmit}>
+          <div style={{ marginBottom: "1rem" }}>
+            <input
+              type="text"
+              name="name"
+              value={workoutPlan.name}
+              onChange={handleInputChange}
+              placeholder="Workout Name"
+            />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <input
+              type="text"
+              name="description"
+              value={workoutPlan.description}
+              onChange={handleInputChange}
+              placeholder="Workout Description"
+            />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <input
+              type="text"
+              name="muscleType"
+              value={workoutPlan.muscleType}
+              onChange={handleInputChange}
+              placeholder="Muscle Type"
+            />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <input
+              type="text"
+              name="duration"
+              value={workoutPlan.duration}
+              onChange={handleInputChange}
+              placeholder="Workout Duration"
+            />
+          </div>
 
-      <h2>Load Workout Plan</h2>
-      <form onSubmit={handleSearch}>
-        {/* ... input fields for loading workout plan ... */}
-      </form>
-      {/* ... display loaded workout plan ... */}
-      {loading
-        ? "Loading..."
-        : error
-        ? `Error! ${error.message}`
-        : data && (
-            <div>
-              <h1>{data.workoutPlan.name}</h1>
-              <p>{data.workoutPlan.description}</p>
-              <p>{data.workoutPlan.muscleType}</p>
-              <p>{data.workoutPlan.duration}</p>
-              {data.workoutPlan.exercises.map((exercise, index) => (
-                <div key={index}>
-                  <h2>{exercise.name}</h2>
-                  <p>{exercise.description}</p>
-                  <p>Sets: {exercise.sets}</p>
-                  <p>Reps: {exercise.reps}</p>
-                  <p>Duration: {exercise.duration}</p>
-                </div>
+          {/* Input fields for exercises */}
+          <div style={{ marginBottom: "1rem" }}>
+            <input
+              type="text"
+              name="exerciseName"
+              value={exercise.name}
+              onChange={handleExerciseChange}
+              placeholder="Exercise Name"
+            />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <input
+              type="text"
+              name="exerciseDescription"
+              value={exercise.description}
+              onChange={handleExerciseChange}
+              placeholder="Exercise Description"
+            />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <input
+              type="number"
+              name="exerciseSets"
+              value={exercise.sets}
+              onChange={handleExerciseChange}
+              placeholder="Sets"
+            />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <input
+              type="number"
+              name="exerciseReps"
+              value={exercise.reps}
+              onChange={handleExerciseChange}
+              placeholder="Reps"
+            />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <input
+              type="number"
+              name="exerciseDuration"
+              value={exercise.duration}
+              onChange={handleExerciseChange}
+              placeholder="Exercise Duration"
+            />
+          </div>
+
+          <button type="button" onClick={handleAddExercise}>
+            Add Exercise
+          </button>
+          <button type="submit">Create Workout Plan</button>
+        </form>
+      </div>
+
+      <div style={{ flex: "1" }}>
+        <h2>Load Workout Plan</h2>
+        <form onSubmit={handleSearch}>
+          {/* ... input fields for loading workout plan ... */}
+        </form>
+
+        {/* List of existing workout plans */}
+        <div>
+          {loading ? (
+            "Loading..."
+          ) : error ? (
+            `Error! ${error.message}`
+          ) : (
+            <ul>
+              {data.workoutPlans.map((workoutPlan) => (
+                <li
+                  key={workoutPlan.id}
+                  onClick={() => handleWorkoutPlanClick(workoutPlan)}
+                  style={{ cursor: "pointer", marginBottom: "0.5rem" }}
+                >
+                  {workoutPlan.name}
+                </li>
               ))}
-            </div>
+            </ul>
           )}
+        </div>
+
+        {/* Display selected workout plan */}
+        {selectedWorkoutPlan && (
+          <div>
+            <h1>{selectedWorkoutPlan.name}</h1>
+            <p>{selectedWorkoutPlan.description}</p>
+            <p>{selectedWorkoutPlan.muscleType}</p>
+            <p>{selectedWorkoutPlan.duration}</p>
+            {selectedWorkoutPlan.exercises.map((exercise, index) => (
+              <div key={index}>
+                <h2>{exercise.name}</h2>
+                <p>{exercise.description}</p>
+                <p>Sets: {exercise.sets}</p>
+                <p>Reps: {exercise.reps}</p>
+                <p>Duration: {exercise.duration}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
