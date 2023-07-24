23109fbda62c5174536c246e1524a28ca0a7e4ac
Sean L <leonardmsean@gmail.com>
2023-07-24 19:11:13 -0400

    added contactus schema

---
+++ server/models/ContactUs.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const contactUsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ContactUs = mongoose.model("ContactUs", contactUsSchema);
+
+module.exports = ContactUs;
